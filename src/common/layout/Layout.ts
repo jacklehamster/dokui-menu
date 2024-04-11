b1b91f485fa14f3a8d548bcaffae726eb4ccdd27
Jack Le Hamster <jacklehamster@gmail.com>
2024-04-10 23:37:25 -0700

    nullable size / position.

--- src/common/layout/Layout.ts
+++ src/common/layout/Layout.ts
@@ -1,7 +1,7 @@
 export interface LayoutModel {
   name?: string;
-  position?: [number | undefined, number | undefined];
-  size?: [number | undefined, number | undefined];
+  position?: [number | undefined | null, number | undefined | null];
+  size?: [number | undefined | null, number | undefined | null];
   positionFromRight?: boolean;
   positionFromBottom?: boolean;
 }
--- src/common/layout/usePopupLayout.ts
+++ src/common/layout/usePopupLayout.ts
@@ -13,8 +13,8 @@ interface Props {
 export function usePopupLayout({ layout, setVisible }: Props) {
   const { getLayout, uniqueLayout } = useLayoutContext();
   const layoutModel = getLayout(layout);
-  const x = layoutModel.position?.[0] || DEFAULT_HORIZONTAL_PADDING;
-  const y = layoutModel.position?.[1] || DEFAULT_VERTICAL_PADDING;
+  const x = layoutModel.position?.[0] ?? DEFAULT_HORIZONTAL_PADDING;
+  const y = layoutModel.position?.[1] ?? DEFAULT_VERTICAL_PADDING;
   const left = layoutModel.positionFromRight ? `calc(100% - ${x}px)` : x;
   const top = layoutModel.positionFromBottom ? `calc(100% - ${y}px)` : y;
   const right = DEFAULT_HORIZONTAL_PADDING;
